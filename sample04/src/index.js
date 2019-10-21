3e7aef8af7e6810420a1d55a118cb1522877278d
Jun Kurihara <kurihara@ieee.org>
2019-10-21 20:57:31 +0900

    updated

--- sample04/src/index.js
+++ sample04/src/index.js
@@ -1,8 +1,8 @@
 /**
  * index.js
  */
-import {genHash, genHmac, verifyHmac, genRsaKey} from './test-apis';
+import {genHash, genHmac, verifyHmac, genRsaKey, signRsaPss, verifyRsaPss, genEccKey, signEcdsa, verifyEcdsa} from './test-apis';
 import * as util from './util/format';
 
-export {util, genHash, genHmac, verifyHmac, genRsaKey};
-export default {util, genHash, genHmac, verifyHmac, genRsaKey};
+export {util, genHash, genHmac, verifyHmac, genRsaKey, signRsaPss, verifyRsaPss, genEccKey, signEcdsa, verifyEcdsa};
+export default {util, genHash, genHmac, verifyHmac, genRsaKey, signRsaPss, verifyRsaPss, genEccKey, signEcdsa, verifyEcdsa};
